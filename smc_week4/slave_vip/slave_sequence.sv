ec2a4d3ff4b1718eb03ada8b56265be47dc74e8f
sumin <smchae7@gmail.com>
2025-11-11 13:50:35 +0900

    slave update all

--- smc_week4/slave_vip/slave_sequence.sv
+++ smc_week4/slave_vip/slave_sequence.sv
@@ -1,75 +1,53 @@
 class slave_sequence extends uvm_sequence #(packet);
+	// UVM Factory //
 	`uvm_object_utils(slave_sequence)
-	
+
+	// Type-Safe p_sequencer Declaration //
 	`uvm_declare_p_sequencer(slave_sequencer)
 
+	// Create Environment //
 	int unsigned m_mem[int unsigned];
-
 	packet req;
 	packet rsp;
 	
+	// Constructor //
 	function new(string name = "slave_sequence");
 		super.new(name);
 		req = packet::type_id::create("req");
 		rsp = packet::type_id::create("rsp");
-		
 	endfunction
 
-
-
-
-
-	virtual task body(); //sequence는 body에 run_phase 코드를 작성
-		
-		//packet req,rsp;
+	// Simulation Execution //
+	virtual task body();
 		forever begin
 			p_sequencer.request_fifo.get(req);
-			//`uvm_info("SLV_SEQ",$sformatf("data=%p,addr=%h, read=%0b, write=%0b, size=%0d",req.data, req.addr, req.read, req.write , req.size),UVM_LOW)
+			`uvm_info("SLV_SEQ",$sformatf("data=%p,addr=%h, read=%0b, write=%0b, size=%0d",req.data, req.addr, req.read, req.write , req.size),UVM_MEDIUM)
+			rsp.addr		= req.addr;
+			rsp.size		= req.size;
+			rsp.read		= req.read;
+			rsp.write		= req.write;
+			rsp.data		= new[req.size];
+			rsp.error		= 0;
+			rsp.wait_state	= new[req.size];
 
-
-			rsp = packet::type_id::create("rsp");
-			rsp.addr = req.addr;
-			rsp.size = req.size;
-			rsp.read = req.read;
-			rsp.write = req.write;
-			rsp.data = new[req.size];
-			rsp.error = 0;
-			rsp.wait_state= new[req.size];
-		
 			for (int i=0 ; i< req.size; i++) begin
-				rsp.wait_state[i] =1;
+				rsp.wait_state[i] = 1;
 				if (req.write) begin
+					`uvm_info("SLV_SEQ", "Sequence WRITE DATA Bring", UVM_MEDIUM)
 					m_mem[req.addr+i] = req.data[i];
 				end
 				if (req.read) begin
-					if(!m_mem.exists(req.addr + i))
-						m_mem[req.addr +i] = $urandom_range(8'h10,8'hFF);
-					rsp.data[i] = m_mem[req.addr + i];
+					`uvm_info("SLV_SEQ", "Sequnece READ DATA Bring", UVM_MEDIUM)
+					if(!m_mem.exists(req.addr + i)) begin
+						m_mem[req.addr +i] = $urandom_range(8'h10, 8'hFF);
+						rsp.data[i] = m_mem[req.addr + i];
+					end
 				end
 			end
-			//req.print();
-			start_item(rsp);  // ready to transfer
-			//`uvm_info("SEQ", $sformatf("Randomized tr: %s", tr.sprint()), UVM_LOW)
-			`uvm_info("SLV_SEQ",$sformatf("data=%p,addr=%h, read=%0b, write=%0b, size=%0d",rsp.data, req.addr, req.read, req.write , req.size),UVM_LOW)
-
+			start_item(rsp);
 			finish_item(rsp);
-                        //`uvm_info("SEQ", $sformatf("Randomized tr: %s", tr.sprint()), UVM_LOW)
-
-
+			`uvm_info("SLV_SEQ", "Sequence Finish Item", UVM_MEDIUM)
 		end
-		//`uvm_info("SEQ", $sformatf("Randomized tr: %s", tr.sprint()), UVM_LOW)
+		`uvm_info("SLV_SEQ", "RUN Phase Check", UVM_MEDIUM)
 	endtask	
-		
-	/*virtual task post_body(); // body 태스크 실행 직후에 호출
-        // body가 완료되면 objection을 내려 시뮬레이션이 종료될 수 있도록 합니다.
-        	uvm_phase starting_phase = get_starting_phase();
-       		if (starting_phase != null) begin
-            		starting_phase.drop_objection(this);
-        	end
-   	 endtask*/
-
-
-
-
-
 endclass 
