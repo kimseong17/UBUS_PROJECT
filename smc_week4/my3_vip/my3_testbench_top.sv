7244cc816be233049dcb132a35009dbe854b5f99
내 이름 <gitlab_admin_a1e386@example.com>
2025-11-11 10:55:04 +0900

    week4_6

---
+++ smc_week4/my3_vip/my3_testbench_top.sv
@@ -0,0 +1,51 @@
+module my3_testbench_top;
+    //`include "uvm_macros.svh" 
+    import uvm_pkg::*;
+    `include "packet.sv"
+    
+    `include "ubus_scoreboard.sv"
+
+    `include "../master_vip/master_sequence.sv"
+    
+    
+    `include "../master_vip/master_monitor.sv"
+    `include "../master_vip/master_driver.sv"
+    `include "../master_vip/master_sequencer.sv"
+    `include "../master_vip/master_agent.sv"
+
+    `include "../slave_vip/slave_sequencer.sv"
+    `include "../slave_vip/slave_sequence.sv"
+    `include "../slave_vip/slave_monitor.sv"
+    `include "../slave_vip/slave_driver.sv"
+    `include "../slave_vip/slave_agent.sv" 
+
+
+
+    `include "ubus_master_write_word_seq.sv"
+    `include "ubus_master_read_word_seq.sv"
+    `include "my3_vip_environment.sv"
+    `include "ubus_write_test.sv"
+
+    // Interface Instance
+    ubus_if vif();
+    // Clock Generation
+    initial vif.ubus_clock = 0;
+    always #5 vif.ubus_clock = ~vif.ubus_clock;
+
+    //uvm_config_db#(virtual ubus_if)::set(null,"*","vif",vif);
+    // 구성요소: context, inst_name , field_name, vif
+    // null-> root에서 시작 , hierarchy의 모든 component에 적용, component에서 get할 때 사용 , 실제 interface instance 
+
+	
+
+    initial begin 
+	$fsdbDumpfile("wave.fsdb");
+	$fsdbDumpvars(0, my3_testbench_top);
+	
+	uvm_config_db#(virtual ubus_if.MASTER)::set(null,"*","vif",vif.MASTER);
+	uvm_config_db#(virtual ubus_if.SLAVE)::set(null,"*","vif",vif.SLAVE);
+        run_test();
+    end
+
+
+endmodule
